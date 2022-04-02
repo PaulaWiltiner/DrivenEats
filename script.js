0a5852001a4991c30444f95fccd54af1f691ffdd
Paula Wiltiner <paulawiltiner.pw@gmail.com>
2022-04-02 19:43:33 -0300

    bonus 1

--- script.js
+++ script.js
@@ -71,5 +71,12 @@ function selecionarSobremesa(opcao) {
 }
 
 function fazerPedido(){
-    window.open(` https://web.whatsapp.com/send?phone=5575988164646&text=${encodeURIComponent("Olá, gostaria de fazer o pedido: \n - Prato: Frango Yin Yang \n - Bebida: Coquinha Gelada \n - Sobremesa: Pudim \n Total: R$ 27.70")}`);
+
+    let nome =prompt("Por favor, digite o seu nome:");
+    let endereco = prompt("Por favor, digite o seu endereço:");
+    if (nome!=="" && endereco !==""){
+        const padrao = "Olá, gostaria de fazer o pedido: \n - Prato: Frango Yin Yang \n - Bebida: Coquinha Gelada \n - Sobremesa: Pudim \n Total: R$ 27.70";
+        const texto= padrao + '\n\n' + "Nome: " + nome + '\n' + "Endereço: " + endereco;
+        window.open(` https://web.whatsapp.com/send?phone=5575988164646&text=${encodeURIComponent(texto)}`);
+    }
 }
